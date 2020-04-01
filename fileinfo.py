42b22fe6629ac8560261c0bcdf306185b39cbe9d
Your Name <you@example.com>
2020-04-01 12:06:47 +0800

    update by the second developer

--- fileinfo.py
+++ fileinfo.py
@@ -48,3 +48,97 @@ elif f[-1] != '1' and f[-1] != '9' and len(f) == 13 and flag == True:
             li.append(s)
             break                 #以上五种情况考虑的是最后摸上来的牌是九万的情况，此时考虑一万的数量，与之前的最后摸到的是一万的情况类似
 
+flag = False                     #万牌的情况考虑结束，开始考虑“筒”牌的胡牌情况，方法与“万”牌类似
+for i in range(len(c)):
+    if c[i] == 'p':
+        g.append(c[i - 1])
+    if g.count('1') >= 3 and '2' in g and '3' in g and '4' in g and '5' in g and '6' in g and '7' in g and '8' in g and g.count('9')>=3:
+        d = i
+        flag = True
+        break
+if flag == True:
+    if g[-1] == '1' and g.count('9')>3 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif g[-1] == '1' and g.count('9') == 3 and len(g) > 13 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif g[-1] == '1' and g.count('9') == 3 and len(g) == 13 and d < len(c) :
+        for i in range(d + 1,len(c)):
+            if c[i] == 'p':
+                s = int((i - 25)/2)
+                li.append(s)
+                break
+    elif g[-1] == '9' and g.count('1')>3 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif g[-1] == '9' and g.count('1') == 3 and d < len(c) and len(g) == 13 :
+        for i in range(d + 1,len(c)):
+            if c[i] == 'p':
+                s = int((i - 25)/2)
+                li.append(s)
+                break
+    elif g[-1] == '9' and g.count('1') == 3 and len(g) > 13 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif g[-1] != '1' and g[-1] != '9' and len(g) > 13:
+        s = int((d - 25)/2)
+        li.append(s)
+    elif g[-1] != '1' and g[-1] != '9' and len(g) == 13 :
+        for i in range(d + 1,len(c)):
+            if c[i] == 'p':
+                s = int((i - 25)/2)
+                li.append(s)
+                break       
+
+
+
+
+
+
+flag = False                   #用相同方法考虑“条”牌的胡牌情况
+for i in range(len(c)):
+    if c[i] == 's':
+        h.append(c[i - 1])
+    if h.count('1') >= 3 and '2' in h and '3' in h and '4' in h and '5' in h and '6' in h and '7' in h and '8' in h and h.count('9')>=3:
+        d = i
+        flag = True
+        break
+if flag == True:
+    if h[-1] == '1' and h.count('9')>3 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif h[-1] == '1' and h.count('9') == 3 and len(h) > 13 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif h[-1] == '1' and h.count('9') == 3 and len(h) == 13 and d < len(c) :
+        for i in range(d + 1,len(c)):
+            if c[i] == 's':
+                s = int((i - 25)/2)
+                li.append(s)
+                break
+    elif h[-1] == '9' and h.count('1')>3 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif h[-1] == '9' and h.count('1') == 3 and d < len(c) and len(h) == 13 :
+        for i in range(d + 1,len(c)):
+            if c[i] == 's':
+                s = int((i - 25)/2)
+                li.append(s)
+                break
+    elif h[-1] == '9' and h.count('1') == 3 and len(h) > 13 :
+        s = int((d - 25)/2)
+        li.append(s)
+    elif h[-1] != '1' and h[-1] != '9' and len(h) > 13:
+        s = int((d - 25)/2)
+        li.append(s)
+    elif h[-1] != '1' and h[-1] != '9' and len(h) == 13 :
+        for i in range(d + 1,len(c)):
+            if c[i] == 's':
+                s = int((i - 25)/2)
+                li.append(s)
+                break
+if len(li) != 0:
+    print(min(li))        #若能胡牌，输出最先能使胡牌的那张牌的位置
+else:
+    print('Stop Your Daydream!')
